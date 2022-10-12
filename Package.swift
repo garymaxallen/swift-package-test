c10cc95dd0164e27e498cbd00ed17e5b909a0f63
root <root@mymail.com>
2022-10-12 16:02:48 +0800

    xxx

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+  name: "MySwift",
+  products: [
+    .executable(name: "MySwift", targets: ["MySwift"])
+  ],
+  dependencies: [],
+  targets: [
+    .executableTarget(
+      name: "MySwift",
+      dependencies: [
+        .target(name: "MyPoint")
+      ]
+    ),
+    .target(
+      name: "MyPoint"
+    ),
+  ]
+)
---
+++ Sources/MySwift/main.swift
@@ -0,0 +1,5 @@
+import MyPoint
+
+print("++++++++++++++++++ Hello, world +++++++++++++++++++++++++")
+let p = MyPoint(x: 4, y: 20)
+print("Hello, world!", p.x, p.y)
