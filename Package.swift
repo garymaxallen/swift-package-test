1bfea841c4fedbb184c136cdbf7a2deb8986fcd9
unknown <598369987@qq.com>
2022-10-14 17:26:32 +0800

    xxx

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "MySwift",
-    products: [
-        .executable(name: "MySwift", targets: ["MySwift"]),
-    ],
-    targets: [
-        .executableTarget(
-            name: "MySwift",
-            dependencies: [
-                .target(name: "MyC"),
-            ]
-        ),
-        .target(
-            name: "MyC"
-        ),
-    ]
+  name: "MySwift",
+  products: [
+    .executable(name: "MySwift", targets: ["MySwift"])
+  ],
+  targets: [
+    .executableTarget(
+      name: "MySwift",
+      dependencies: [
+        .target(name: "MyC")
+      ]
+    ),
+    .target(
+      name: "MyC"
+    ),
+  ]
 )
